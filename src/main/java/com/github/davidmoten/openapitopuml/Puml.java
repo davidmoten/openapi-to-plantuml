7059ca679d4756a5507c947b3f9671c6fdaed0f3
David Moten <davidmoten@gmail.com>
2021-03-19 12:25:51 +1100

    dev

--- src/main/java/com/github/davidmoten/openapitopuml/Puml.java
+++ src/main/java/com/github/davidmoten/openapitopuml/Puml.java
@@ -1,25 +1,74 @@
 package com.github.davidmoten.openapitopuml;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
 import io.swagger.parser.OpenAPIParser;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.media.ObjectSchema;
+import io.swagger.v3.oas.models.media.Schema;
+import io.swagger.v3.oas.models.media.StringSchema;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 
 public class Puml {
-    
+
     public static String toPuml(String openApi) {
-        SwaggerParseResult result = new OpenAPIParser().readContents(openApi,null, null);
-        
+        SwaggerParseResult result = new OpenAPIParser().readContents(openApi, null, null);
+
         // or from a file
-        //   SwaggerParseResult result = new OpenAPIParser().readContents("./path/to/openapi.yaml", null, null);
-        
+        // SwaggerParseResult result = new
+        // OpenAPIParser().readContents("./path/to/openapi.yaml", null, null);
+
         // the parsed POJO
         OpenAPI a = result.getOpenAPI();
-        
-        a.getComponents().getSchemas().entrySet().stream();
-        return null;
+
+        // @startuml
+//        class A {
+//            {static} int counter
+//            +void {abstract} start(int timeout)
+//            }
+//            note right of A::counter
+//              This member is annotated
+//            end note
+//            note right of A::start
+//              This method is now explained in a UML note
+//            end note
+//            @enduml
+        return "@startuml\n" //
+                + a.getComponents() //
+                        .getSchemas() //
+                        .entrySet() //
+                        .stream() //
+                        .map(entry -> toPlantUmlClass(entry.getKey(), entry.getValue())) //
+                        .collect(Collectors.joining("\n\n")) //
+                + "\n@enduml";
     }
-    
-    
-    
+
+    private static String toPlantUmlClass(String name, Schema<?> schema) {
+        StringBuilder b = new StringBuilder();
+        List<Entry<String, Schema>> more = new ArrayList<>();
+        b.append("class " + name + "{\n");
+        if (schema instanceof ObjectSchema) {
+            ObjectSchema s = (ObjectSchema) schema;
+            s.getProperties().entrySet().forEach(entry -> {
+                if (entry.getValue() instanceof StringSchema) {
+                    b.append("String " + entry.getKey() + "\n");
+                }
+                more.add(entry);
+            });
+        } else if (schema instanceof StringSchema) {
+            StringSchema s = (StringSchema) schema;
+            b.append("  String value\n");
+        }
+        b.append("}");
+        for (Entry<String, Schema> entry: more) {
+            b.append("\n");
+            b.append(toPlantUmlClass(entry.getKey(), entry.getValue()));
+            b.append("\n");
+        }
+        return b.toString();
+    }
 
 }
--- src/test/java/com/github/davidmoten/openapitopuml/PumlTest.java
+++ src/test/java/com/github/davidmoten/openapitopuml/PumlTest.java
@@ -7,26 +7,16 @@ public class PumlTest {
     @Test
     public void testConvert() {
         String openapi = "openapi: 3.0.1\n" + 
-                "servers:\n" + 
-                "- url: /\n" + 
-                "paths:\n" + 
-                "  /newPerson:\n" + 
-                "    post:\n" + 
-                "      summary: Create new person\n" + 
-                "      description: Create new person\n" + 
-                "      responses:\n" + 
-                "        200:\n" + 
-                "          description: ok\n" + 
-                "          content:\n" + 
-                "            '*/*':\n" + 
-                "              schema:\n" + 
-                "                type: string\n" + 
-                "                example: Example value\n" + 
                 "components:\n" + 
                 "  schemas:\n" + 
                 "    CustomerType:\n" + 
                 "      type: string\n" + 
-                "      example: Example value";
+                "      example: Example value" + 
+                "    Customer:\n" + 
+                "      type: string" +
+                "      "
+                ;
+        
         System.out.println(Puml.toPuml(openapi));
     }
 
