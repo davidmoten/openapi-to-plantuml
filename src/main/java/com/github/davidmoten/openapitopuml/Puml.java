c26e86389cf96c542d869ca9c65d3657bf70d72a
David Moten <davidmoten@gmail.com>
2021-03-20 09:37:07 +1100

    implement paths

--- src/main/java/com/github/davidmoten/openapitopuml/Puml.java
+++ src/main/java/com/github/davidmoten/openapitopuml/Puml.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map.Entry;
 import java.util.Set;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.apache.commons.io.IOUtils;
 
@@ -25,10 +24,12 @@ import io.swagger.v3.oas.models.media.BooleanSchema;
 import io.swagger.v3.oas.models.media.DateSchema;
 import io.swagger.v3.oas.models.media.DateTimeSchema;
 import io.swagger.v3.oas.models.media.IntegerSchema;
+import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.media.NumberSchema;
 import io.swagger.v3.oas.models.media.ObjectSchema;
 import io.swagger.v3.oas.models.media.Schema;
 import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
 import io.swagger.v3.parser.core.models.SwaggerParseResult;
 
 public class Puml {
@@ -48,17 +49,23 @@ public class Puml {
         OpenAPI a = result.getOpenAPI();
 
         return "@startuml" //
+                + "hide <<Method>> circle" //
                 + components(a) //
                 + paths(a) //
                 + "\n@enduml";
     }
 
     private static String paths(OpenAPI a) {
-        return a.getPaths() //
-                .entrySet() //
-                .stream() //
-                .map(entry -> toPlantUmlPath(entry.getKey(), entry.getValue())).collect(Collectors.joining());
-
+        if (a.getPaths() == null) {
+            return "";
+        } else {
+            return a.getPaths() //
+                    .entrySet() //
+                    .stream() //
+                    .map(entry -> toPlantUmlPath(entry.getKey(), //
+                            entry.getValue()))
+                    .collect(Collectors.joining());
+        }
     }
 
     private static String components(OpenAPI a) {
@@ -67,20 +74,73 @@ public class Puml {
                 .entrySet() //
                 .stream() //
                 .map(entry -> toPlantUmlClass(entry.getKey(), entry.getValue())) //
-                .collect(Collectors.joining()) //
-        ;
+                .collect(Collectors.joining());
     }
 
-    private static String toPlantUmlPath(String name, PathItem p) {
+    private static String toPlantUmlPath(String path, PathItem p) {
+        StringBuilder b = new StringBuilder();
         // add method class blocks with HTTP verb and parameters
         // add response lines
-        return operations(p).stream().map(o -> "").collect(Collectors.joining());
+        b.append(p.readOperationsMap() //
+                .entrySet() //
+                .stream() //
+                .map(entry -> {
+                    Operation operation = entry.getValue();
+                    String className = entry.getKey() + " " + path;
+                    StringBuilder s = new StringBuilder();
+                    s.append("\n\nclass \"" + className + "\" <<Method>> {");
+                    List<Parameter> parameters = operation.getParameters();
+                    if (parameters != null) {
+                        s.append(parameters //
+                                .stream()//
+                                .map(param -> {
+                                    final String type = paramToTypeName(param.get$ref(), param.getSchema());
+                                    final String optional = param.getRequired() ? "" : " {O}";
+                                    return "\n" + "  " + param.getName() + " : " + type + optional;
+                                }) //
+                                .collect(Collectors.joining()));
+                    }
+                    s.append("\n}");
+                    s.append(operation //
+                            .getResponses() //
+                            .entrySet() //
+                            .stream() //
+                            .map(ent -> {
+                                String responseCode = ent.getKey();
+                                //TODO only using the first content
+                                Entry<String, MediaType> mediaType = ent.getValue().getContent().entrySet().parallelStream().findFirst().get();
+                                String returnClassName = refToClassName(mediaType.getValue().getSchema().get$ref());
+                                return "\n\n\"" + className + "\" --> \"" + returnClassName + "\": " + responseCode ;
+                            }).collect(Collectors.joining()));
+                    return s.toString();
+                }) //
+                .collect(Collectors.joining()));
+        return b.toString();
     }
 
-    private static List<Operation> operations(PathItem p) {
-        return Stream.of(p.getGet(), p.getPost(), p.getPut(), p.getPatch()) //
-                .filter(o -> o != null) //
-                .collect(Collectors.toList());
+    private static String paramToTypeName(String ref, Schema<?> schema) {
+        final String type;
+        if (ref != null) {
+            type = refToClassName(ref);
+        } else if (schema instanceof StringSchema) {
+            type = "string";
+        } else if (schema instanceof BooleanSchema) {
+            type = "boolean";
+        } else if (schema instanceof DateTimeSchema) {
+            type = "timestamp";
+        } else if (schema instanceof DateSchema) {
+            type = "date";
+        } else if (schema instanceof NumberSchema) {
+            type = "decimal";
+        } else if (schema instanceof IntegerSchema) {
+            type = "integer";
+        } else if (schema instanceof ArraySchema) {
+            ArraySchema a = (ArraySchema) schema;
+            type = paramToTypeName(schema.get$ref(), a.getItems()) + "[]";
+        } else {
+            type = "unknown";
+        }
+        return type;
     }
 
     private static String toPlantUmlClass(String name, Schema<?> schema) {
