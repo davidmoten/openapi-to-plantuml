873282eb70b904339182e992382c29d63940c8ad
David Moten <davidmoten@gmail.com>
2021-03-22 21:04:23 +1100

    dev

--- src/test/java/com/github/davidmoten/oa2puml/v3/DemoBatchTest.java
+++ src/test/java/com/github/davidmoten/oa2puml/v3/DemoBatchTest.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
