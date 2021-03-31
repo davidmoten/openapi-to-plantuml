b4fb85358df9687c05d8eedaee6ed96f5a63ff9d
David Moten <davidmoten@gmail.com>
2021-03-31 12:51:33 +1100

    dev

--- src/test/java/com/github/davidmoten/oas3/internal/NamesTest.java
+++ src/test/java/com/github/davidmoten/oas3/internal/NamesTest.java
@@ -1,6 +1,18 @@
 package com.github.davidmoten.oas3.internal;
 
-public class NamesTest {
-    
+import org.junit.Test;
 
+import io.swagger.v3.oas.models.OpenAPI;
+
+public class NamesTest {
+
+    @Test(expected = RuntimeException.class)
+    public void testRefToClassNameNotFound() {
+        emptyNames().refToClassName("abc");
+    }
+
+    private static Names emptyNames() {
+        OpenAPI a = new OpenAPI();
+        return new Names(a);
+    }
 }
