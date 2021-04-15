abd2f95a39b24b11eb11d6e9315a8e81101fbcf5
Dave Moten <davidmoten@gmail.com>
2021-04-16 07:35:37 +1000

    checkstyle fix

--- src/test/java/com/github/davidmoten/oas3/puml/DeflaterTest.java
+++ src/test/java/com/github/davidmoten/oas3/puml/DeflaterTest.java
@@ -2,22 +2,24 @@ package com.github.davidmoten.oas3.puml;
 
 import static org.junit.Assert.assertTrue;
 
-import java.io.UnsupportedEncodingException;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.util.zip.DataFormatException;
 
 import org.junit.Test;
 
-import net.sourceforge.plantuml.code.NoPlantumlCompressionException;
 import net.sourceforge.plantuml.code.TranscoderSmart2;
 
 public class DeflaterTest {
 
     @Test
-    public void test() throws DataFormatException, UnsupportedEncodingException,
-            NoPlantumlCompressionException {
-        String encodedUml = "XP1DYy8m48Rlyok6dbQN7FOg7YfwKF2q29v3EzXXavdIfCgY_dTRsog27xUyvCdpJYODqLQnzWuWSdI4l-HiP9LGS1dGuDpP4731TbTP3m1Pbm_a7CiEZu3ulPA8MvPS3w6DU-KSrvhzRGfQg5PV8pWF3sTbK-T9Of-NMWVgptFrlfOXTSAXhz40t5gd9zFSYRdh9hYIWYgELZ9w0lRkJzXrd1TGyfFWsDIbmSHR-K_w2IUjkzJ0xTRUqEqN7bb8IV9czHS0";
+    public void test() throws DataFormatException, IOException {
+        String encodedUml = Files.readString(new File("src/test/resources/encodedUml.txt").toPath(),
+                StandardCharsets.UTF_8);
         assertTrue(new TranscoderSmart2().decode(encodedUml).startsWith("@startuml"));
-        //System.out.println("https://planttext.com/api/plantuml/img/" + encodedUml);
+        // System.out.println("https://planttext.com/api/plantuml/img/" + encodedUml);
     }
 
 }
