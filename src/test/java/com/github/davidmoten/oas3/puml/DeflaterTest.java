e080fb5024c25f76b43743ae8df93e3b24272133
Dave Moten <davidmoten@gmail.com>
2021-05-18 20:31:42 +1000

    upgrade plantuml from 1.2021.4 to 1.2021.6

--- src/test/java/com/github/davidmoten/oas3/puml/DeflaterTest.java
+++ src/test/java/com/github/davidmoten/oas3/puml/DeflaterTest.java
@@ -10,21 +10,21 @@ import org.junit.Test;
 
 import com.github.davidmoten.junit.Files;
 
-import net.sourceforge.plantuml.code.TranscoderSmart2;
+import net.sourceforge.plantuml.code.TranscoderSmart;
 
 public class DeflaterTest {
 
     @Test
     public void test() throws DataFormatException, IOException {
         String encodedUml = Files.readUtf8(new File("src/test/resources/encodedUml.txt"));
-        assertTrue(new TranscoderSmart2().decode(encodedUml).startsWith("@startuml"));
+        assertTrue(new TranscoderSmart().decode(encodedUml).startsWith("@startuml"));
         // System.out.println("https://planttext.com/api/plantuml/img/" + encodedUml);
     }
 
     @Test
     public void testEncode() throws IOException {
         String uml = "@startuml\nBob->Alice: hello\n@enduml";
-        String path = new TranscoderSmart2().encode(uml);
+        String path = new TranscoderSmart().encode(uml);
         System.out.println("https://planttext.com/api/plantuml/img/" + path);
     }
 
