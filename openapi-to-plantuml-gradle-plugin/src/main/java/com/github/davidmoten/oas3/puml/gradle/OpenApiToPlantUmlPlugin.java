5f9f4f2bca4f3b2109e0559848973d4af6b45560
Dave Moten <davidmoten@gmail.com>
2025-07-01 13:34:39 +1000

    dev

--- openapi-to-plantuml-gradle-plugin/src/main/java/com/github/davidmoten/oas3/puml/gradle/OpenApiToPlantUmlPlugin.java
+++ openapi-to-plantuml-gradle-plugin/src/main/java/com/github/davidmoten/oas3/puml/gradle/OpenApiToPlantUmlPlugin.java
@@ -20,8 +20,8 @@ import com.github.davidmoten.oas3.puml.Style;
 public class OpenApiToPlantUmlPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
-        OpenApiToPlantUmlExtension extension = project.getExtensions()
-            .create("openApiToPlantUml", OpenApiToPlantUmlExtension.class);
+        OpenApiToPlantUmlExtension extension = project.getExtensions().create("openApiToPlantUml",
+                OpenApiToPlantUmlExtension.class);
 
         project.getTasks().register("generatePlantUml", OpenApiToPlantUmlTask.class, task -> {
             task.setGroup("documentation");
@@ -36,120 +36,124 @@ public class OpenApiToPlantUmlPlugin implements Plugin<Project> {
         });
     }
 
-class OpenApiToPlantUmlExtension {
-    private String style = "SPLIT";
-    private File input;
-    private List<String> formats = List.of("PNG", "SVG");
-    private File output;
-
-    public String getStyle() {
-        return style;
-    }
-
-    public void setStyle(String style) {
-        this.style = style;
-    }
-
-    public File getInput() {
-        return input;
-    }
-
-    public void setInput(File input) {
-        this.input = input;
-    }
-
-    public List<String> getFormats() {
-        return formats;
-    }
-
-    public void setFormats(List<String> formats) {
-        this.formats = formats;
-    }
-
-    public File getOutput() {
-        return output;
-    }
-
-    public void setOutput(File output) {
-        this.output = output;
-    }
-}
-
-abstract class OpenApiToPlantUmlTask extends DefaultTask {
-    private String style;
-    private File input;
-    private List<String> formats = List.of("SVG");
-    private File output;
-
-    @Input
-    public String getStyle() {
-        return style;
-    }
-
-    public void setStyle(String style) {
-        this.style = style;
-    }
-
-    @Input
-    public File getInput() {
-        return input;
-    }
-
-    public void setInput(File input) {
-        this.input = input;
-    }
-
-    @Input
-    public List<String> getFormats() {
-        return formats;
-    }
-
-    public void setFormats(List<String> formats) {
-        this.formats = formats;
-    }
-
-    @Optional
-    @Input
-    public File getOutput() {
-        return output;
-    }
-
-    public void setOutput(File output) {
-        this.output = output;
-    }
-
-    @TaskAction
-    public void generate() {
-        Style resolvedStyle = Style.valueOf(style.toUpperCase(Locale.ENGLISH));
+    public static class OpenApiToPlantUmlExtension {
+        private String style = "SPLIT";
+        private File input;
+        private List<String> formats = List.of("PNG", "SVG");
+        private File output;
         
-        if (formats == null || formats.isEmpty()) {
-            formats = Arrays.asList("PNG", "SVG");
+        public OpenApiToPlantUmlExtension() {
+            
         }
 
-        for (String format : formats) {
-            final File out;
-            if (output == null) {
-                if (resolvedStyle == Style.SINGLE) {
-                    out = new File("build" + File.separator + "diagrams" + File.separator
-                            + "class-diagram." + format.toLowerCase(Locale.ROOT));
-                } else {
-                    out = new File("build" + File.separator + "diagrams");
-                }
-            } else {
-                out = output;
+        public String getStyle() {
+            return style;
+        }
+
+        public void setStyle(String style) {
+            this.style = style;
+        }
+
+        public File getInput() {
+            return input;
+        }
+
+        public void setInput(File input) {
+            this.input = input;
+        }
+
+        public List<String> getFormats() {
+            return formats;
+        }
+
+        public void setFormats(List<String> formats) {
+            this.formats = formats;
+        }
+
+        public File getOutput() {
+            return output;
+        }
+
+        public void setOutput(File output) {
+            this.output = output;
+        }
+    }
+
+    abstract static class OpenApiToPlantUmlTask extends DefaultTask {
+        private String style;
+        private File input;
+        private List<String> formats = List.of("SVG");
+        private File output;
+
+        @Input
+        public String getStyle() {
+            return style;
+        }
+
+        public void setStyle(String style) {
+            this.style = style;
+        }
+
+        @Input
+        public File getInput() {
+            return input;
+        }
+
+        public void setInput(File input) {
+            this.input = input;
+        }
+
+        @Input
+        public List<String> getFormats() {
+            return formats;
+        }
+
+        public void setFormats(List<String> formats) {
+            this.formats = formats;
+        }
+
+        @Optional
+        @Input
+        public File getOutput() {
+            return output;
+        }
+
+        public void setOutput(File output) {
+            this.output = output;
+        }
+
+        @TaskAction
+        public void generate() {
+            Style resolvedStyle = Style.valueOf(style.toUpperCase(Locale.ENGLISH));
+
+            if (formats == null || formats.isEmpty()) {
+                formats = Arrays.asList("PNG", "SVG");
             }
-            getLogger().info("Generating diagram in format=" + format //
-                    + " with style=" + style + " to " + out);
-            try {
-                if (resolvedStyle == Style.SINGLE) {
-                    Converter.writeSingleFile(input, format, out);
+
+            for (String format : formats) {
+                final File out;
+                if (output == null) {
+                    if (resolvedStyle == Style.SINGLE) {
+                        out = new File("build" + File.separator + "diagrams" + File.separator + "class-diagram."
+                                + format.toLowerCase(Locale.ROOT));
+                    } else {
+                        out = new File("build" + File.separator + "diagrams");
+                    }
                 } else {
-                    Converter.writeSplitFiles(input, format, out);
+                    out = output;
                 }
-            } catch (IOException e) {
-                throw new UncheckedIOException("Error generating diagrams", e);
+                getLogger().info("Generating diagram in format=" + format //
+                        + " with style=" + style + " to " + out);
+                try {
+                    if (resolvedStyle == Style.SINGLE) {
+                        Converter.writeSingleFile(input, format, out);
+                    } else {
+                        Converter.writeSplitFiles(input, format, out);
+                    }
+                } catch (IOException e) {
+                    throw new UncheckedIOException("Error generating diagrams", e);
+                }
             }
         }
     }
-}
 }
